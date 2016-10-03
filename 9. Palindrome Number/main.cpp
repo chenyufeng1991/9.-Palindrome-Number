652d3e1de789bc19a6a3e74fa776bf9ad75dda30
陈宇峰 <yufengcode@gmail.com>
2016-10-03 22:42:14 +0800

    init

--- 9. Palindrome Number/main.cpp
+++ 9. Palindrome Number/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
+#include <string>
+#include <sstream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+bool isPalindrome(int x)
+{
+    // 负数不是回文
+    if (x < 0)
+    {
+        return false;
+    }
+
+    string inputStr;
+    ostringstream os;
+    os << x;
+    inputStr = os.str();
+
+    string reverseInputStr = inputStr;
+    reverse(reverseInputStr.begin(), reverseInputStr.end());
+
+    if (reverseInputStr == inputStr)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+int main(int argc, const char * argv[])
+{
     return 0;
 }
